fe6d0583b2df79ff4e310fe09a037087742af50a
sadbanned <sadpinkys@mail.ru>
2022-10-03 22:53:30 +0600

    Все страницы и компоненты

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Basket from './pages/basketPage/Basket';
+import Catalog from './pages/catalogPage/Catalog';
+import Contacts from './pages/contactsPage/Contacts';
+import Favorite from './pages/favoritePage/Favorite';
+import Home from './pages/homePage/Home';
+import Payment from './pages/paymentPage/Payment';
+import Product from './pages/productPage/Product';
+import Return from './pages/returnPage/Return';
+import History from './pages/historyPage/History'
+import Data from './pages/dataPage/Data';
+import Registr from './pages/regPage/Registr';
+import Login from './pages/loginPage/Login'
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home/>}/>
+                <Route path='/catalog' element={<Catalog/>}/>
+                <Route path='/exchange_return' element={<Return/>}/>
+                <Route path='/payment_delivery' element={<Payment/>}/>
+                <Route path='/product' element={<Product/>}/>
+                <Route path='/basket_clearance' element={<Basket/>}/>
+                <Route path='/favorite' element={<Favorite/>}/>
+                <Route path='/contacts' element={<Contacts/>}/>
+                <Route path='/history_products' element={<History/>}/>
+                <Route path='/personal_data' element={<Data/>}/>
+                <Route path='/registration' element={<Registr/>}/>
+                <Route path='/login' element={<Login/>}/>
+
             </Routes>
         </BrowserRouter>
     );
