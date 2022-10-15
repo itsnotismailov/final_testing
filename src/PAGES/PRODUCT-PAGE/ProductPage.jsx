ffac54c25a2afdaa0cf8948a800748faa48f56b3
itsnotismailov <ismailov10800@gmail.com>
2022-10-15 21:28:25 +0600

    Update ProductPage.jsx

--- src/PAGES/PRODUCT-PAGE/ProductPage.jsx
+++ src/PAGES/PRODUCT-PAGE/ProductPage.jsx
@@ -59,7 +59,6 @@ const ProductPage = ({ handleClick }) => {
   return (
     <div className='productpage'>
         {/* <Navbar />` */}
-        {/* <Navbar /> */}
         <div className='switcher'>
           <Link to='/'>Главная</Link>
           <img src={switcher} alt='' />
